081d3af7fb2dce71be74f8bd7f2c1a5c581d1736
Imdeveloper14 <164135466+Imdeveloper14@users.noreply.github.com>
2026-07-02 15:43:26 +0530

    inital commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Primalisometric Holtrop Calculator Started");
+
+    initializeApplication();
+
+});
+
+function initializeApplication(){
+
+    console.log("Loading modules...");
+
+}
